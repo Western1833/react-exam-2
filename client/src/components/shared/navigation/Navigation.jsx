94694b9340a77fe4cecc466be24af55578a4b4fd
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-05-12 22:21:30 +0300

    Added navigation component and css

---
+++ client/src/components/shared/navigation/Navigation.jsx
@@ -0,0 +1,40 @@
+import '../navigation/nav-styles.css';
+
+const Navigation = () => {
+    return (
+        <header class="site-section site-header">
+            <div class="site-logo">
+                <a routerLink="/">
+                    <img src="assets/remade-logo-2.png" alt="Logo" />
+                </a>
+                <a routerLink="/"><span>CarSales</span></a>
+            </div>
+            <nav>
+                <ul>
+                    {/* <li *ngIf="userDataInStorage"><span>Hello, {{userDataInStorage.username}}</span></li>
+            <li><a routerLink="/">Home</a></li>
+            <li><a routerLink="/data/catalog">Catalog</a></li>
+            <!-- logged in users -->
+            <li *ngIf="userDataInStorage"><a routerLink="/data/add-car">Add car</a></li>
+            <li *ngIf="userDataInStorage"><a routerLink="/data/my-cars">My cars</a></li>
+            <li *ngIf="userDataInStorage"><a (click)="logout()" routerLink="/logout">Logout</a></li>
+            <!-- logged out users -->
+            <li *ngIf="!userDataInStorage"><a routerLink="/login">Login</a></li>
+            <li *ngIf="!userDataInStorage"><a routerLink="/register">Register</a></li> */}
+                    <li><span>Hello, Iliya</span></li>
+                    <li><a>Home</a></li>
+                    <li><a>Catalog</a></li>
+
+                    <li><a>Add car</a></li>
+                    <li><a>My cars</a></li>
+                    <li><a>Logout</a></li>
+
+                    <li><a>Login</a></li>
+                    <li><a>Register</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Navigation;
