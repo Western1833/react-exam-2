18a1265d20affdcbdc5adb47fc73e71c194b740a
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-05-15 23:24:27 +0300

    Update on navigation

--- client/src/components/shared/navigation/Navigation.jsx
+++ client/src/components/shared/navigation/Navigation.jsx
@@ -1,13 +1,14 @@
 import '../navigation/nav-styles.css';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <header class="site-section site-header">
             <div class="site-logo">
-                <a href="/">
+                <Link to="/">
                     <img src="remade-logo-2.png" alt="Logo" />
-                </a>
-                <a href="/"><span>CarSales</span></a>
+                </Link>
+                <Link to="/"><span>CarSales</span></Link>
             </div>
             <nav>
                 <ul>
@@ -22,14 +23,14 @@ const Navigation = () => {
             <li *ngIf="!userDataInStorage"><a routerLink="/login">Login</a></li>
             <li *ngIf="!userDataInStorage"><a routerLink="/register">Register</a></li> */}
                     <li><span>Hello, Iliya</span></li>
-                    <li><a href='/'>Home</a></li>
+                    <li><Link to='/'>Home</Link></li>
                     <li><a>Catalog</a></li>
 
                     <li><a>Add car</a></li>
                     <li><a>My cars</a></li>
                     <li><a>Logout</a></li>
 
-                    <li><a href='/login'>Login</a></li>
+                    <li><Link to='/login'>Login</Link></li>
                     <li><a>Register</a></li>
                 </ul>
             </nav>
