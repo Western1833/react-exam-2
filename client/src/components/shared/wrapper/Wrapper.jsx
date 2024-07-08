2afe178e8254330bbb83455adecfc7293a7d6713
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-07-08 14:30:11 +0300

    Added authGuard

--- client/src/components/shared/wrapper/Wrapper.jsx
+++ client/src/components/shared/wrapper/Wrapper.jsx
@@ -5,6 +5,7 @@ import AllCars from '../../cars-functionalities/all-cars/All-cars.jsx';
 import CreateCar from '../../cars-functionalities/create-car/Create-car.jsx';
 import { Details } from '../../cars-functionalities/details/Details.jsx';
 import LastThreeCars from '../../cars-functionalities/last-three-cars/Last-three-cars.jsx';
+import AuthGuard from '../../guards/AuthGuard.jsx';
 import '../wrapper/wrapper-styles.css';
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,7 +16,7 @@ const Wrapper = () => {
                     <Route path='/' element={<LastThreeCars />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
-                    <Route path='/add-car' element={<CreateCar />} />
+                    <Route path='/add-car' element={<AuthGuard><CreateCar /></AuthGuard>} />
                     <Route path='/all-cars' element={<AllCars />} />
                     <Route path='/details/:id' element={<Details />} />
                     <Route path='/users/logout'element={<Logout />} />
